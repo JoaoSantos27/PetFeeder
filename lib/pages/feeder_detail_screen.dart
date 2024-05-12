dc715af3cb50e4ed698e9d7cffce126fd99c9a0e
JoaoSantos27 <jb.santos@campus.fct.unl.pt>
2024-05-12 17:48:46 +0100

    Fix to photo feature

--- lib/pages/feeder_detail_screen.dart
+++ lib/pages/feeder_detail_screen.dart
@@ -12,37 +12,43 @@ class FeederDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Feeder Details'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  'Name: ${feeder.name}',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  'Description: ${feeder.description}',
-                  style: TextStyle(fontSize: 16),
-                ),
-                SizedBox(height: 20),
-                if (feeder.imageFile != null) // Display image if available
-                  Container(
-                    height: 500, // Set the maximum height
-                    width: double.infinity, // Set the maximum width
-                    child: Image.file(
-                      feeder.imageFile!,
-                      fit: BoxFit.cover, // Adjusts the size of the image to cover the entire container
-                    ),
-                  ),
-              ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            // Image displayed to the left
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+                image: feeder.imageFile != null ? DecorationImage(
+                  image: FileImage(feeder.imageFile!),
+                  fit: BoxFit.cover,
+                ) : null, // Set image to null if feeder.imageFile is null
+              ),
             ),
-          ),
-        ],
+            SizedBox(width: 16), // Add some spacing between the image and text
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Name: ${feeder.name}',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Description: ${feeder.description}',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  SizedBox(height: 20),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
